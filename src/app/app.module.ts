874c3436a224d403a1c17064c30cf6ba9c0010e2
liangjiacong <liangjiacong>
2019-12-04 16:00:21 +0800

    项目打包之后，部署到服务器，刷新访问404解决方法（tips：已删除上一版方案 [32eb6a6]）

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+// import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,10 +29,12 @@ registerLocaleData(zh);
     BrowserAnimationsModule
 
   ],
-  providers: [{ provide: NZ_I18N, useValue: zh_CN }, {
-    provide: LocationStrategy,
-    useClass: HashLocationStrategy
-  }],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }
+  //   , {
+  //   provide: LocationStrategy,
+  //   useClass: HashLocationStrategy
+  // }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
