85a00bb19550e96072dff4e33605bd0689e7411a
liangjiacong <liangjiacong>
2019-12-19 16:22:53 +0800

    添加material design（含主题 和icon）

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// 引入material和hammer
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+
+import 'hammerjs';
+
+// 引入flex-layout
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    MatButtonModule, // 为子组件引入 Material Button
+    MatSelectModule, MatIconModule,
+    FlexLayoutModule, // 为子组件引入 FlexLayout
   ],
   providers: [],
   bootstrap: [AppComponent]
